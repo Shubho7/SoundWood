bb81649894c226bf2ffcd66d98cc25ddd6cb7285
Shubho7 <banerjeesubhodeep54@gmail.com>
2024-11-07 02:42:29 +0530

    unnecessary libraries removed

--- models/QA.py
+++ models/QA.py
@@ -3,8 +3,6 @@ from transformers import BertTokenizer, BertForQuestionAnswering
 import whisper
 import pandas as pd
 import gtts  
-import os
-import playsound
 import sounddevice as sd
 import numpy as np
 import scipy.io.wavfile as wav
