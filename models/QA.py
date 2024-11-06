3709b95d39cbd1b9861c7019ce3c7ddf0bec313f
Shubho7 <banerjeesubhodeep54@gmail.com>
2024-11-07 01:21:10 +0530

    asr model loaded

--- models/QA.py
+++ models/QA.py
@@ -8,3 +8,10 @@ import playsound
 import sounddevice as sd
 import numpy as np
 import scipy.io.wavfile as wav
+
+# Load Whisper Model for Transcription
+whisper_model = whisper.load_model("models\whisper-modelv1")
+
+# Load Multilingual BERT Model for Question Answering
+qa_model = BertForQuestionAnswering.from_pretrained("bert-base-multilingual-cased")
+tokenizer = BertTokenizer.from_pretrained("bert-base-multilingual-cased")
