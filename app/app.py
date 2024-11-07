e6acec2aeecc8f30fb2eebc6f0b468ec354d9e7c
Shubho7 <banerjeesubhodeep54@gmail.com>
2024-11-08 01:02:15 +0530

    req function imported

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import tempfile
-from models.QA import transcribe_audio, search_answers, transcriptions_df, 
+from models.QA import transcribe_audio, search_answers, transcriptions_df, text_to_speech
 
 st.title("SoundWood")
 st.write("Record your question about sandalwood cultivation:")
