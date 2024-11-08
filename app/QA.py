627b5a28e15fc8683e587c8985dae7abce31bb48
Shubho7 <banerjeesubhodeep54@gmail.com>
2024-11-08 17:44:53 +0530

    modifying whisper files

--- app/QA.py
+++ app/QA.py
@@ -8,7 +8,7 @@ import tempfile
 import scipy.io.wavfile as wav
 
 # Load Whisper model for transcription
-whisper_model = r"C:\Users\baner\Documents\SoundWood\models\whisper-modelv1"
+whisper_model = r"C:\Users\baner\Documents\SoundWood\finetuned_saved\whisper-modelv1"
 whisper_processor = WhisperProcessor.from_pretrained(whisper_model)
 whisper_model_QA = WhisperForConditionalGeneration.from_pretrained(whisper_model)
 
